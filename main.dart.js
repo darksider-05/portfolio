df7470005c4c0548a2e4ff63a8aed8cd8fac28ec
darksider-05 <darksider-05@users.noreply.github.com>
2026-02-19 11:12:05 +0000

    deploy: 53662ab39baf23a1fe31331a43a2ac476fe71a39

--- main.dart.js
+++ main.dart.js
@@ -81641,7 +81641,7 @@ uH(a,b,c,d){return this.Xo(a,b,c,d)},
 Xo(a,b,c,d){var s=0,r=A.O(t.H),q=1,p=[],o=this,n,m,l,k,j,i,h,g
 var $async$uH=A.P(function(e,f){if(e===1){p.push(f)
 s=q}for(;;)switch(s){case 0:k=A.hp("https://portfolio-relay.darksider-05.workers.dev/")
-j=b==="Ng.notgiven@gmail.com"?"Not Given":b
+j=b===""?"Not Given":b
 i=t.N
 h=A.ad(["name",a,"email",j,"message","Subject:  "+c+"\n\n"+d],i,t.z)
 q=3
